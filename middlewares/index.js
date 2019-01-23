9234ef586b1a40d127d18ae6915df3c1216336c8
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-23 15:42:25 +0700

    making decoded for verifying token

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,26 @@
-exports.isAuthenticated = (req, res, next) => {
+const jwt = require("jsonwebtoken");
+const User = require("../models").User;
+
+exports.isAuthenticated = async (req, res, next) => {
   const token =
     req.headers.authorization && req.headers.authorization.split(" ")[1];
 
   if (token === undefined) {
     return res.send("token not found");
   }
-  res.send(token);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ where: { id: decoded.id } });
+
+    if (user === null) {
+      return res.send("Account NOT found");
+    }
+
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    res.send("error");
+  }
+  //res.send(token) : get token
 };
